f19b9e36a7428ad83f356d286f41fac66ae0265a
Forrest4643 <90162968+Forrest4643@users.noreply.github.com>
2023-01-17 20:57:13 -0800

    Added PID heading correction and field oriented drive

--- src/main/java/frc/robot/commands/cartesianMecanumDrive.java
+++ src/main/java/frc/robot/commands/cartesianMecanumDrive.java
@@ -6,22 +6,34 @@ package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Sensors;
 
 
 public class cartesianMecanumDrive extends CommandBase {
 
   private final DriveSubsystem m_driveSubsystem;
   private final DoubleSupplier speedX, speedY, rotationSpeed;
+  private PIDController driveHeadingController = new PIDController(0.003, 0, 0.0031);
+  private Sensors m_sensors;
+
+  private double m_expectedHeading;
+
 
   /** Creates a new cartesianMecanumDrive. */
-  public cartesianMecanumDrive(DriveSubsystem m_driveSubsystem, DoubleSupplier speedX, DoubleSupplier speedY, DoubleSupplier rotationSpeed) {
+  public cartesianMecanumDrive(DriveSubsystem m_driveSubsystem, Sensors m_sensors, DoubleSupplier speedX, DoubleSupplier speedY, DoubleSupplier rotationSpeed) {
     this.m_driveSubsystem = m_driveSubsystem;
+    this.m_sensors = m_sensors;
     this.speedX = speedX;
     this.speedY = speedY;
     this.rotationSpeed = rotationSpeed;
+
+    m_expectedHeading = 0;
+
+    driveHeadingController.enableContinuousInput(0, 360);
     addRequirements(m_driveSubsystem);
   }
   // Called when the command is initially scheduled.
@@ -31,7 +43,12 @@ public class cartesianMecanumDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_driveSubsystem.cartesianMecanumDrive(speedX, speedY, rotationSpeed);
+
+    m_expectedHeading = m_expectedHeading + rotationSpeed.getAsDouble();    
+
+    double rotationOutput = driveHeadingController.calculate(m_sensors.navXYaw(), m_expectedHeading);    
+
+    m_driveSubsystem.cartesianMecanumDrive(speedX, speedY, () -> rotationOutput);
 
     SmartDashboard.putNumber("speedX", speedX.getAsDouble());
     SmartDashboard.putNumber("speedY", speedY.getAsDouble());
